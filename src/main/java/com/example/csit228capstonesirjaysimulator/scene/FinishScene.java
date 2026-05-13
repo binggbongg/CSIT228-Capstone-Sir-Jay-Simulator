9840c18627071a9a95ab7c5fcd2fa4b06ba4c2a7
Sophia <snlogarta@gmail.com>
2026-05-13 13:52:07 +0800

    small fix for finish scene

--- src/main/java/com/example/csit228capstonesirjaysimulator/scene/FinishScene.java
+++ src/main/java/com/example/csit228capstonesirjaysimulator/scene/FinishScene.java
@@ -50,6 +50,7 @@ public class FinishScene extends SubScene {
         // Pull down Background 3
         Image imgBg3 = new Image("assets/textures/game-over-bg-3.png", 1280, 0, true, true);
         ImageView bg3 = new ImageView(imgBg3);
+        bg3.setTranslateX(-300);
         bg3.setTranslateY(-imgBg3.getHeight());
 
         TranslateTransition pullDownBg3 = new TranslateTransition(Duration.seconds(1), bg3);
